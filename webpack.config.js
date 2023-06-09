dcfd76b6e1e3954ed3277d6f42e6b00f2f318f47
Thang <cmthang2407@gmail.com>
2023-06-09 14:10:53 +0700

    update webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('./html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: './src',
+        open: true,
+        port: 2407,
+    },
+    module: {
+        rules: [
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'app.css',
+        }),
+    ],
+    mode: 'development',
+}
